32c3193531780fc4417326bc7f279f9df7e16f2b
dv16sen <dv16sen@cs.umu.se>
2019-07-27 16:19:30 +0200

    Made the build script copy over all .scss files.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 node updateExports.js
 rm -rf dist
-./node_modules/.bin/babel ./src/module --out-dir dist -s inline
+./node_modules/.bin/babel ./src/module --out-dir dist -s inline --copy-files
 cp -r package.json dist
 cp -r LICENCE dist
 cp -r README.md dist
